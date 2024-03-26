f9f234c0db523442a9364c09ab24ba6a83376804
DanPlayz0 <damuffin36@gmail.com>
2024-03-25 19:04:41 -0700

    Fix websocket url query params (1.3.2)

--- src/Server.ts
+++ src/Server.ts
@@ -260,7 +260,7 @@ export class Server<Context extends {}, Methods extends LocalRouteMethods<Contex
     });
     this.server = server;
     server.on('upgrade', (request, socket, head) => {
-      if (this.config.websocket.enabled == false || this.config.websocket.path !== request.url) {
+      if (this.config.websocket.enabled == false || new URL(request.url!).host == this.config.websocket.path ) {
         socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
         socket.destroy();
         return;
